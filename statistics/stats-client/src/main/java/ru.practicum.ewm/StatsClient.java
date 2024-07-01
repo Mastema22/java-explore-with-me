b722fa3db931a13faf98ce5d69790e2bb3a714e1
Eugeni <eugenibelosheikin@yandex.ru>
2024-07-02 00:30:48 +0300

    Исправил StatsRepository

--- statistics/stats-client/src/main/java/ru.practicum.ewm/StatsClient.java
+++ statistics/stats-client/src/main/java/ru.practicum.ewm/StatsClient.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import ru.practicum.ewm.request.EndpointHitDto;
@@ -12,11 +12,10 @@ import ru.practicum.ewm.request.EndpointHitDto;
 import java.util.List;
 import java.util.Map;
 
-@Service
 public class StatsClient extends BaseClient {
 
     @Autowired
-    public StatsClient(@Value("${statistic.url}") String serviceUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${stats-service.url}") String serviceUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serviceUrl))
