21ab98e9a6e34cad3369e067453ea085e2da0e03
Eugeni <eugenibelosheikin@yandex.ru>
2024-07-01 23:55:56 +0300

    Исправил StatsRepository

--- statistics/stats-service/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
+++ statistics/stats-service/src/main/java/ru/practicum/ewm/exceptions/ErrorHandler.java
@@ -3,9 +3,7 @@ package ru.practicum.ewm.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
