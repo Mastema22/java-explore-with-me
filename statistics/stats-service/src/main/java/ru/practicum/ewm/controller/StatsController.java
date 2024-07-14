6677c2f8ce592044070b08a7bd069cb383d4976d
Eugeni <eugenibelosheikin@yandex.ru>
2024-07-14 18:21:05 +0300

    corrected StatsServiceImpl

--- statistics/stats-service/src/main/java/ru/practicum/ewm/controller/StatsController.java
+++ statistics/stats-service/src/main/java/ru/practicum/ewm/controller/StatsController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.ewm.exceptions.DateNotValidationException;
 import ru.practicum.ewm.request.EndpointHitDto;
 import ru.practicum.ewm.response.ViewStatsDto;
 import ru.practicum.ewm.service.StatsService;
@@ -29,7 +28,6 @@ public class StatsController {
                                        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                        @RequestParam(value = "uris", required = false) List<String> uris,
                                        @RequestParam(value = "unique", defaultValue = "false") Boolean unique) {
-        validateParam(start, end);
         List<ViewStatsDto> viewStatsList = statsService.getStats(start, end, uris, unique);
         log.info("Statistics collection is completed successfully!");
         return viewStatsList;
@@ -42,15 +40,5 @@ public class StatsController {
         EndpointHitDto endpointHitDtoResult = statsService.saveHit(endpointHitDto);
         log.info("The data has been saved successfully!");
         return endpointHitDtoResult;
-    }
-
-    private void validateParam(LocalDateTime startDate, LocalDateTime endDate) {
-        if (startDate == null || endDate == null) {
-            throw new DateNotValidationException("Start date or end date cannot be null!");
-        }
-
-        if (startDate.isAfter(endDate)) {
-            throw new DateNotValidationException("Start date is after end date - checked!");
-        }
     }
 }
--- statistics/stats-service/src/main/java/ru/practicum/ewm/model/ViewStats.java
+++ statistics/stats-service/src/main/java/ru/practicum/ewm/model/ViewStats.java
@@ -11,7 +11,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ViewStats {
-
     private String app;
     private String uri;
     private Long hits;
